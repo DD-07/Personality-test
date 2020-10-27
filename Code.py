4af020768ad6c8fc1e1f78398d96fc8ad2be579e
DT <69886702+DD-07@users.noreply.github.com>
2020-10-27 12:18:22 +0800

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,1 +1,16 @@
+# Personality test!
+#Based on:
+"""
+Sanguine personality type is described primarily as being highly talkative, enthusiastic, active, and social. Sanguines tend to be more extroverted and enjoy being part of a crowd; they find that being social, outgoing, and charismatic is easy to accomplish.[2][3] Individuals with this personality have a hard time doing nothing and engage in more risk seeking behavior.[2]
+
+Choleric individuals tend to be more extroverted. They are described as independent, decisive, goal-oriented, and ambitious. These combined with their dominant, result-oriented outlook make them natural leaders. In Greek, Medieval, and Renaissance thought, they were also violent, vengeful, and short-tempered.[19]
+
+Melancholic individuals tend to be analytical and detail-oriented, and they are deep thinkers and feelers. They are introverted and try to avoid being singled out in a crowd.[2] A melancholic personality leads to self-reliant individuals who are thoughtful, reserved, and often anxious.[2] They often strive for perfection within themselves and their surroundings, which leads to tidy and detail-oriented behavior.[2]
+
+Phlegmatic individuals tend to be relaxed, peaceful, quiet, and easy-going.[2] They are sympathetic and care about others, yet they try to hide their emotions. Phlegmatic individuals are also good at generalising ideas or problems to the world and making compromises.[2]
+
+"""
+
+# Start of quiz
+# Start the code below this line. :)
 
